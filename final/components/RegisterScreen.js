7859da451a721e650a071b6d9c23d1586a7cb15c
Sebastian Hallabrin <72759108+bashalla@users.noreply.github.com>
2023-12-25 20:01:14 +0100

    checking all and finishing

--- final/components/RegisterScreen.js
+++ final/components/RegisterScreen.js
@@ -1,4 +1,3 @@
-// RegisterScreen.js
 import React, { useState } from "react";
 import {
   KeyboardAvoidingView,
@@ -17,13 +16,21 @@ import { doc, setDoc } from "firebase/firestore";
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); // New state for password confirmation
   const [firstName, setFirstName] = useState("");
-  const [birthdate, setBirthdate] = useState("");
+  const [name, setName] = useState("");
   const [nationality, setNationality] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState("");
   const navigation = useNavigation();
 
   const handleSignUp = () => {
+    // Check if passwords match
+    if (password !== confirmPassword) {
+      Alert.alert("Password Mismatch", "Passwords do not match.");
+      return; // Prevent registration if passwords don't match
+    }
+
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
@@ -34,12 +41,13 @@ const RegisterScreen = () => {
         setDoc(userRef, {
           name: name,
           firstName: firstName,
-          birthdate: birthdate,
           nationality: nationality,
+          gender: gender,
+          age: age,
         })
           .then(() => {
             // Handle successful registration
-            navigation.replace("Home");
+            navigation.navigate("Home");
           })
           .catch((error) => {
             // Handle any errors
@@ -66,6 +74,13 @@ const RegisterScreen = () => {
           secureTextEntry
         />
         <TextInput
+          placeholder="Confirm Password" // New input for password confirmation
+          value={confirmPassword}
+          onChangeText={(text) => setConfirmPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+        <TextInput
           placeholder="First Name"
           value={firstName}
           onChangeText={(text) => setFirstName(text)}
@@ -78,16 +93,41 @@ const RegisterScreen = () => {
           style={styles.input}
         />
         <TextInput
-          placeholder="Birthdate (YYYY-MM-DD)"
-          value={birthdate}
-          onChangeText={(text) => setBirthdate(text)}
-          style={styles.input}
-        />
-        <TextInput
           placeholder="Nationality"
           value={nationality}
           onChangeText={(text) => setNationality(text)}
           style={styles.input}
+        />
+        <Text>Gender:</Text>
+        <View style={styles.radioContainer}>
+          <TouchableOpacity
+            style={styles.radioButton}
+            onPress={() => setGender("male")}
+          >
+            <Text>Male</Text>
+            {gender === "male" && <View style={styles.radioDot} />}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.radioButton}
+            onPress={() => setGender("female")}
+          >
+            <Text>Female</Text>
+            {gender === "female" && <View style={styles.radioDot} />}
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.radioButton}
+            onPress={() => setGender("other")}
+          >
+            <Text>Other</Text>
+            {gender === "other" && <View style={styles.radioDot} />}
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          style={styles.input}
+          value={age}
+          onChangeText={(text) => setAge(text)}
+          placeholder="Enter your age"
+          keyboardType="numeric"
         />
       </View>
       <TouchableOpacity onPress={handleSignUp} style={styles.button}>
@@ -111,7 +151,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
-    marginTop: 5,
+    marginTop: 10, // Increase the top margin for better spacing
   },
   button: {
     backgroundColor: "#0782F9",
@@ -125,6 +165,22 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "700",
     fontSize: 16,
+  },
+  radioContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10, // Increase the top margin for better spacing
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  radioDot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "#0782F9", // Change this to your desired color
+    marginLeft: 5,
   },
 });
 
