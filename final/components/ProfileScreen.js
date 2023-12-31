2cccfef62b93ed7ce906e29717c5fdeda26fb3f4
Sebastian Hallabrin <72759108+bashalla@users.noreply.github.com>
2023-12-31 14:54:07 +0100

    adjustement

--- final/components/ProfileScreen.js
+++ final/components/ProfileScreen.js
@@ -7,14 +7,19 @@ import {
   TouchableOpacity,
   Image,
   Alert,
+  Modal,
+  KeyboardAvoidingView,
+  ScrollView,
 } from "react-native";
 import { auth, db, storage } from "../firebase";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { updatePassword } from "firebase/auth";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import * as ImagePicker from "expo-image-picker";
 import placeholderImage from "../assets/adaptive-icon.png";
 import * as ImageManipulator from "expo-image-manipulator";
-// Adjust the path to your placeholder image
+import CountryPicker from "react-native-country-picker-modal";
+import { Picker } from "@react-native-picker/picker";
 
 const resizeImage = async (imageUri) => {
   try {
@@ -40,6 +45,13 @@ function ProfileScreen() {
   });
   const [profileImage, setProfileImage] = useState(null);
   const [newPassword, setNewPassword] = useState("");
+  const [countryPickerVisible, setCountryPickerVisible] = useState(false);
+  const [agePickerVisible, setAgePickerVisible] = useState(false);
+
+  const handleCountrySelect = (country) => {
+    setUserData({ ...userData, nationality: country.name });
+    setCountryPickerVisible(false);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -73,7 +85,7 @@ function ProfileScreen() {
       quality: 1,
     });
 
-    // Exit if selection is cancelled
+    // Exit if selection is canceled
     if (result.canceled) {
       return;
     }
@@ -147,66 +159,144 @@ function ProfileScreen() {
   };
 
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={handleSelectImage}>
-        <Image
-          source={profileImage ? { uri: profileImage } : placeholderImage}
-          style={styles.profileImage}
-          resizeMode="cover"
-        />
-      </TouchableOpacity>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <TouchableOpacity onPress={handleSelectImage}>
+          <Image
+            source={profileImage ? { uri: profileImage } : placeholderImage}
+            style={styles.profileImage}
+            resizeMode="cover"
+          />
+        </TouchableOpacity>
 
-      <TextInput
-        style={styles.input}
-        value={userData.firstName}
-        onChangeText={(text) => setUserData({ ...userData, firstName: text })}
-        placeholder="First Name"
-      />
-      <TextInput
-        style={styles.input}
-        value={userData.name}
-        onChangeText={(text) => setUserData({ ...userData, name: text })}
-        placeholder="Last Name"
-      />
-      <TextInput
-        style={styles.input}
-        value={userData.nationality}
-        onChangeText={(text) => setUserData({ ...userData, nationality: text })}
-        placeholder="Nationality"
-      />
-      <TextInput
-        style={styles.input}
-        value={userData.gender}
-        onChangeText={(text) => setUserData({ ...userData, gender: text })}
-        placeholder="Gender"
-      />
-      <TextInput
-        style={styles.input}
-        value={userData.age}
-        onChangeText={(text) => setUserData({ ...userData, age: text })}
-        placeholder="Age"
-        keyboardType="numeric"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleUpdateProfile}>
-        <Text style={styles.buttonText}>Update Profile</Text>
-      </TouchableOpacity>
-      <TextInput
-        style={styles.input}
-        value={newPassword}
-        onChangeText={setNewPassword}
-        placeholder="New Password"
-        secureTextEntry
-      />
-      <TouchableOpacity style={styles.button} onPress={handleChangePassword}>
-        <Text style={styles.buttonText}>Change Password</Text>
-      </TouchableOpacity>
-    </View>
+        <TextInput
+          style={styles.input}
+          value={userData.firstName}
+          onChangeText={(text) => setUserData({ ...userData, firstName: text })}
+          placeholder="First Name"
+        />
+        <TextInput
+          style={styles.input}
+          value={userData.name}
+          onChangeText={(text) => setUserData({ ...userData, name: text })}
+          placeholder="Last Name"
+        />
+
+        {/* Nationality Picker */}
+        <TouchableOpacity
+          onPress={() => setCountryPickerVisible(true)}
+          style={styles.input}
+        >
+          <Text style={styles.inputText}>
+            {userData.nationality || "Select Nationality"}
+          </Text>
+        </TouchableOpacity>
+        <CountryPicker
+          visible={countryPickerVisible}
+          onSelect={handleCountrySelect}
+          onClose={() => setCountryPickerVisible(false)}
+          containerButtonStyle={{ display: "none" }}
+        />
+
+        {/* Age Picker */}
+        <TouchableOpacity
+          onPress={() => setAgePickerVisible(true)}
+          style={styles.input}
+        >
+          <Text style={styles.inputText}>
+            {userData.age ? `Age: ${userData.age}` : "Select Age"}
+          </Text>
+        </TouchableOpacity>
+        <Modal
+          transparent={true}
+          visible={agePickerVisible}
+          onRequestClose={() => setAgePickerVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Picker
+                selectedValue={userData.age}
+                onValueChange={(itemValue) =>
+                  setUserData({ ...userData, age: itemValue })
+                }
+                style={styles.picker}
+              >
+                {Array.from({ length: 100 }, (_, i) => i + 1).map((age) => (
+                  <Picker.Item key={age} label={`${age}`} value={age} />
+                ))}
+              </Picker>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => setAgePickerVisible(false)}
+              >
+                <Text style={styles.modalButtonText}>Done</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+        {/* Gender Selection */}
+        <View style={styles.genderContainer}>
+          <TouchableOpacity
+            onPress={() => setUserData({ ...userData, gender: "male" })}
+            style={[
+              styles.genderButton,
+              userData.gender === "male" ? styles.genderButtonSelected : {},
+            ]}
+          >
+            <Text style={styles.genderText}>Male</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setUserData({ ...userData, gender: "female" })}
+            style={[
+              styles.genderButton,
+              userData.gender === "female" ? styles.genderButtonSelected : {},
+            ]}
+          >
+            <Text style={styles.genderText}>Female</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setUserData({ ...userData, gender: "other" })}
+            style={[
+              styles.genderButton,
+              userData.gender === "other" ? styles.genderButtonSelected : {},
+            ]}
+          >
+            <Text style={styles.genderText}>Other</Text>
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={handleUpdateProfile}>
+          <Text style={styles.buttonText}>Update Profile</Text>
+        </TouchableOpacity>
+
+        <TextInput
+          style={styles.input}
+          value={newPassword}
+          onChangeText={setNewPassword}
+          placeholder="New Password"
+          secureTextEntry // This keeps the text hidden for password privacy
+          placeholderTextColor="gray" // Optionally set placeholder text color
+        />
+        <TouchableOpacity style={styles.button} onPress={handleChangePassword}>
+          <Text style={styles.buttonText}>Change Password</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scrollView: {
+    flexGrow: 1,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -217,12 +307,16 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   input: {
+    backgroundColor: "white", // Ensures a white background
+    paddingHorizontal: 20,
+    paddingVertical: 8,
+    borderRadius: 10,
+    marginVertical: 10,
+    fontSize: 14, // Makes text larger and more visible
+    width: "100%", // Adjust width as per your layout
     borderWidth: 1,
     borderColor: "gray",
-    width: "80%",
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
+    color: "black", // Ensures text color is black for visibility
   },
   button: {
     marginBottom: 20,
@@ -232,6 +326,53 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   buttonText: {
+    color: "white",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center", // Center the modal vertically
+    alignItems: "center", // Center the modal horizontally
+    backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent background
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    width: "80%", // Adjust the width as needed
+    alignItems: "center", // Center the contents
+  },
+  picker: {
+    width: "100%",
+    height: 150,
+  },
+  modalButton: {
+    backgroundColor: "#0782F9",
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 5,
+    width: "50%", // Adjust the width as needed
+    alignItems: "center",
+  },
+  modalButtonText: {
+    color: "white",
+  },
+  genderContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  genderButton: {
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#0782F9",
+  },
+  genderText: {
+    fontSize: 18,
+  },
+  genderButtonSelected: {
+    backgroundColor: "#0782F9",
     color: "white",
   },
 });
