8e7bed594bfacc4ac74c19393de77d006feb9c07
Sebastian Hallabrin <72759108+bashalla@users.noreply.github.com>
2024-01-30 20:14:27 +0100

    Update RewardsScreen.js

--- final/components/RewardsScreen.js
+++ final/components/RewardsScreen.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff", // You can change the background color if needed
+    backgroundColor: "#fff",
   },
   text: {
     fontSize: 20, // Adjust font size as needed
